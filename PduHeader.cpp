01c3f233dc569db2fbbf416b6b182a37601a809c
Dr Dennis Evnagelista <evangeli@usna.edu>
2019-07-29 13:21:35 -0400

    bug fix is->iso forgot the o

--- PduHeader.cpp
+++ PduHeader.cpp
@@ -43,7 +43,7 @@ namespace nmea2k {
        if the PDU Format value is less than 240, then the lower byte 
        of the PGN is set to 0. 
     */
-    unsigned int result = _translation.is.r << 17; 
+    unsigned int result = _translation.iso.r << 17; 
     //MBED_ASSERT(result==0); // r should always be 0
     result += _translation.iso.dp << 16;
     result += _translation.iso.pf << 8; 

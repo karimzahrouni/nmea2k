82f0510e5a9d59ad108ccbeb8e147e174bae5c0f
Dr Dennis Evnagelista <evangeli@usna.edu>
2019-07-31 14:34:03 -0400

    bug fixes

--- TESTS/frame/constructor/main.cpp
+++ TESTS/frame/constructor/main.cpp
@@ -15,7 +15,8 @@
 
 
 Serial pc(USBTX,USBRX); 
-nmea2k::Frame() f; 
+nmea2k::Frame f;
+nmea2k::Frame *gp;
 
 
 
@@ -29,12 +30,8 @@ int main(void){
     pc.printf("f.data[i] = %02x rando junk\r\n",f.data[i]);
   pc.printf("f.len = %d should be 8\r\n",f.len);
   TEST_ASSERT_EQUAL_MESSAGE(8,f.len,"unexpected f.len");
-  TEST_ASSERT_EQUAL_MESSAGE(mbed::CANData,f.type,"unexpected CAN type");
-  TEST_ASSERT_EQUAL_MESSAGE(mbed::CANExtended,f.format,"unexpected CAN format");
-
-  pc.printf("now testing destructor\r\n");
-  delete f;
-  TEST_ASSERT_NULL_MESSAGE(f,"failed destructor");
+  TEST_ASSERT_EQUAL_MESSAGE(CAN::CANData,f.type,"unexpected CAN type");
+  TEST_ASSERT_EQUAL_MESSAGE(CAN::CANExtended,f.format,"unexpected CAN format");
 
   pc.printf("testing constructor with data\r\n");
   char counter = 0; 
@@ -42,11 +39,11 @@ int main(void){
   TEST_ASSERT_EQUAL_MESSAGE(1337,f.id,"failed to set id");
   TEST_ASSERT_EQUAL_MESSAGE(0,f.data[0],"failed to set data");
   TEST_ASSERT_EQUAL_MESSAGE(1,f.len,"failed to set len");
-  TEST_ASSERT_EQUAL_MESSAGE(mbed::CANData,f.type,"unexpected CAN type");
-  TEST_ASSERT_EQUAL_MESSAGE(mbed::CANExtended,f.format,"unexpected CAN format");
+  TEST_ASSERT_EQUAL_MESSAGE(CAN::CANData,f.type,"unexpected CAN type");
+  TEST_ASSERT_EQUAL_MESSAGE(CAN::CANExtended,f.format,"unexpected CAN format");
 
   pc.printf("testing constructor with unsigned char data\r\n");
-  unsigned char counter = {7,6,5,4}; 
+  unsigned char counter[4] = {7,6,5,4}; 
   f = nmea2k::Frame(1337,&counter,4);
   TEST_ASSERT_EQUAL_MESSAGE(1337,f.id,"failed to set id");
   TEST_ASSERT_EQUAL_MESSAGE(7,f.data[0],"failed to set data");
@@ -56,8 +53,11 @@ int main(void){
   TEST_ASSERT_EQUAL_MESSAGE(4,f.len,"failed to set len");
   TEST_ASSERT_EQUAL_MESSAGE(mbed::CANData,f.type,"unexpected CAN type");
   TEST_ASSERT_EQUAL_MESSAGE(mbed::CANExtended,f.format,"unexpected CAN format");
-  
-  pc.printf("once again, i am become shiva, destroyer of worlds\r\n");
-  delete f;
-  TEST_ASSERT_NULL_MESSAGE(f,"failed destructor"); 
+
+  pc.printf("testing constructor\r\n");
+  gp = new nmea2k::Frame;
+  TEST_ASSERT_NOT_NULL_MESSAGE(gp,"failed constructor");
+  pc.printf("testing destructor\r\n");
+  delete gp;
+  TEST_ASSERT_NULL_MESSAGE(gp,"failed destructor"); 
 } // int main(void)

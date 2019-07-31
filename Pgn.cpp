98189562b48f570f518e20b9a6d3faddbc16b850
Dr Dennis Evnagelista <evangeli@usna.edu>
2019-07-31 13:45:05 -0400

    Adding debug and error statements in Pgn.cpp

--- Pgn.cpp
+++ Pgn.cpp
@@ -1,26 +1,35 @@
 #include "mbed.h"
-#include "nmea2k.h"
 #include "Pgn.h"
 
 namespace nmea2k{
 
   Pgn::Pgn(){
+    debug("Pgn() empty constructor called\r\n");
   }
 
   Pgn::~Pgn(){
+    debug("~Pgn() destructor called\r\n");
   }
 
   int Pgn::encode(Pdu &encoded){
+    MBED_ERROR( MBED_MAKE_ERROR(MBED_MODULE_APPLICATION,
+				MBED_ERROR_CODE_INVALID_OPERATION),
+		"Pgn::encode() should be overriden by subclasses" );
     return MBED_ERROR_CODE_INVALID_OPERATION;
   }
 
   PgnParser::PgnParser(){
+    debug("PgnParser() empty constructor called\r\n"); 
   }
 
   PgnParser::~PgnParser(){
+    debug("~PgnParser() destructor called\r\n"); 
   }
 
   int PgnParser::parse(Pdu &encoded, Pgn &decoded){
+    MBED_ERROR( MBED_MAKE_ERROR(MBED_MODULE_APPLICATION,
+				MBED_ERROR_CODE_INVALID_OPERATION),
+		"PgnParser::parse() should be overrided by subclasses");
     return MBED_ERROR_CODE_INVALID_OPERATION;
   }
   

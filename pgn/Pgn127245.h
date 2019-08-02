cc8801841065fc22ea20c7039bbfc432b4d0d488
Dr Dennis Evnagelista <evangeli@usna.edu>
2019-08-02 16:30:35 -0400

    Working on PGN 127245 Rudder

--- pgn/Pgn127245.h
+++ pgn/Pgn127245.h
@@ -2,10 +2,17 @@
 #define PGN127245_H
 
 #include "Pgn.h"
+#include "math.h"
 #include "PduHeader.h"
 #include "Pdu.h"
 
 #define PGN127245_LEN 8
+#define PGN127245_DIR_NONE 0
+#define PGN127245_DIR_RIGHT 1
+#define PGN127245_DIR_LEFT 2
+#define PGN127245_DIR_AMIDSHIPS 3
+#define PGN127245_ANG_NONE 0x7fff
+#define PGN127245_POS_NONE 0x7fff
 
 namespace nmea2k{
 
@@ -13,8 +20,6 @@ namespace nmea2k{
   public:
 
     Pgn127245();
-
-
     
     int encode(PduHeader *h,
 	       Pdu *encoded);
@@ -34,34 +39,36 @@ namespace nmea2k{
     }
 
     // setters (apply scaling, resolution, units). 
-    inline void set_instance(unsigned char x){
+    inline void set_instance(uint8_t x){
       _translation.fields.instance=x;
     }
-    inline void set_direction_order(unsigned char x){
+    inline void set_direction_order(uint8_t x){
       _translation.fields.direction_order = x & 0x3; 
     };
     inline void set_angle_order(float rad){
-      int x = round(10000*rad); 
-      _translation.fields.angle_order[0] = x & 0xff;
-      _translation.fields.angle_order[1] = (x >> 8) & 0xff;
+      int16_t x = round(10000*rad); 
+      _translation.fields.angle_order = x;
     }
     inline void set_position(float rad){
-      int x = round(10000*rad); 
-      _translation.fields.position[0] = x & 0xff;
-      _translation.fields.position[1] = (x >> 8) & 0xff; 
+      int16_t x = round(10000*rad); 
+      _translation.fields.position = x;
     };
 
   private:
     union{
       unsigned char data[PGN127245_LEN] = {0xff,0xff,0xff,0xff,
 					   0xff,0xff,0xff,0xff};
-      struct{
-	uint16t position;
-	uint16t angle_order;
-        unsigned char reserved : 6, 
-	  direction_order : 2,
-	  instance,
-	  unused[2]; 
+      /** @struct _translation
+	  @brief Handy translation table
+	  See this reference: <http://data.over-blog-kiwi.com/0/54/01/67/20151122/ob_f92436_nmea2k-network-design-v2.pdf> 
+       */
+      struct{ // fields in reverse order... 
+	int16_t unused,
+	  position, //<! current angle of rudder in units of 0.0001 rad
+	  angle_order; //<! 0x7fff indicates no angle order present? 
+        uint8_t reserved : 6, 
+	  direction_order : 2, //<! 0 is no direction order 
+	  instance; //<! Identifies rudder instance, 0-251 (did they mean 255?). 
       } fields; 
     } _translation;
   }; // class Pgn127245

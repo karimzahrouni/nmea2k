0e5236bd442d5fe79efd1141926a750dbf737169
Dennis Evangelista <devangel77b@gmail.com>
2019-08-04 18:44:30 -0400

    bleh

--- pgn/Pgn126993.h
+++ pgn/Pgn126993.h
@@ -4,6 +4,7 @@
 #include "PgnData.h"
 
 #define PGN_126993_PRIORITY 7
+#define PGN_126993_DLEN 8
 
 namespace nmea2k{
 
@@ -11,7 +12,10 @@ class Pgn126993:public PgnData{
 public:
   
  Pgn126993(uint16_t interval_100, uint8_t count):
-  PgnData(7,126993,"Heartbeat PGN 126993",8){
+  PgnData(PGN_126993_PRIORITY,
+          126993,
+          "Heartbeat PGN 126993",
+          PGN_126993_DLEN){
     _translation.fields.interval = interval_100;
     _translation.fields.count = count; 
   };

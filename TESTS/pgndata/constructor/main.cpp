150065845d4c8f21587dc9d29fad31b4ca4d710b
Dr Dennis Evnagelista <evangeli@usna.edu>
2019-08-05 09:45:08 -0400

    updating pgndata constructor test

--- TESTS/pgndata/constructor/main.cpp
+++ TESTS/pgndata/constructor/main.cpp
@@ -1,32 +1,36 @@
-/** @file TESTS/pgn/constructor/main.cpp
+/** @file TESTS/pgndata/constructor/main.cpp
     @brief Test generic Pgn constructor
     Dennis Evangelista, 2019
 */
 
-#include "Pgn.h"
+#include "PgnData.h"
 #include "mbed.h"
 #include "stdio.h"
 #include "unity.h"
 
 Serial pc(USBTX,USBRX);
 
-nmea2k::Pgn p;
+nmea2k::PgnData p;
 unsigned char d[8];
 unsigned char l; 
 
 int main(void){
   //pc.printf("nmea2k version ");
   //pc.printf(NMEA2K_VERSION);
-  pc.printf("\r\nnmea2k::Pgn() constructor test\r\n");
+  pc.printf("\r\nnmea2k::PgnData() constructor test\r\n");
   
   pc.printf("Default constructor\r\n");
-  p = nmea2k::Pgn();
-  pc.printf("p.len = %d\r\n",p.len);
-  TEST_ASSERT_EQUAL_MESSAGE(NMEA2K_SINGLE_FRAME_MAX_LEN,
-			    p.len,"incorrect length");
-  p.get_data(d,&l);
-  for (int i=0; i<l; i++)
-    pc.printf("p.data[%d] = %d\r\n",i,d[i]);
-  
-  // can't do much else 
+  p = nmea2k::PgnData();
+  pc.printf("p.p = %d\r\n",p.p);
+  TEST_ASSERT_EQUAL_MESSAGE(7,p.p,"incorrect priority");
+  pc.printf("p.pgn = %d\r\n",p.pgn);
+  TEST_ASSERT_EQUAL_MESSAGE(0,p.pgn,"incorrect pgn"); 
+  pc.printf("p.name = %s\r\n",p.name); 
+  pc.printf("p.dlen = %d\r\n",p.dlen);
+  TEST_ASSERT_EQUAL_MESSAGE(8,
+			    p.dlen,"incorrect length");
+
+  pc.printf("Trying to read data from abstract PgnData\r\n");
+  pc.printf("Next line should throw an error\r\n"); 
+  d = p.data();
 }
